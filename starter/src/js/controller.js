23ab3d244670ebc7c7fc33041bdf6033c843aeec
Yuzbashyev Muhammetaly <megamaster8856@gmail.com>
2023-04-12 17:38:30 +0500

    Cleaned up controller

--- starter/src/js/controller.js
+++ starter/src/js/controller.js
@@ -68,7 +68,6 @@ const controlServings = function (newServings) {
   // Update the recipe servings(in state)
   model.updateServings(newServings);
   //Update the recipe view
-  // recipeView.render(model.state.recipe);
   recipeView.update(model.state.recipe);
 };
 
@@ -128,10 +127,6 @@ const controlAddRecipe = async function (newRecipe) {
 //   }
 // };
 
-const newFeature = function () {
-  console.log('Welcome to the application');
-};
-
 const init = function () {
   bookmarkView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -140,7 +135,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  newFeature();
 };
 init();
 
