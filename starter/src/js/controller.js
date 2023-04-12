914ea499baf8d7d4336c4a23bdf67c2836fe4e0c
Yuzbashyev Muhammetaly <megamaster8856@gmail.com>
2023-04-12 16:04:25 +0500

    Added new welocme feature

--- starter/src/js/controller.js
+++ starter/src/js/controller.js
@@ -127,6 +127,11 @@ const controlAddRecipe = async function (newRecipe) {
 //     addRecipeView.renderError(err.message);
 //   }
 // };
+
+const newFeature = function () {
+  console.log('Welcome to the application');
+};
+
 const init = function () {
   bookmarkView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -135,6 +140,7 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
+  newFeature();
 };
 init();
 
